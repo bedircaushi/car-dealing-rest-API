3d1fdb1e6b659f0fe512ae1d43614023b31630a4
bedircaushi <bedircaushi1@gmail.com>
2019-12-09 18:41:21 +0100

    added new api endpoint for reading

--- models/Car.php
+++ models/Car.php
@@ -40,6 +40,11 @@
             return $query = 'SELECT * FROM ' . $this->table. " c WHERE c.make="."'".$make."' and c.model="."'".$model."'";
             $stmt=$db->prepare($query);
         }
+        //get all cars of specific model, make, mileage, price and registration
+        public function getWithDetails($make,$model,$mileage,$price,$registration){
+            return $query = 'SELECT * FROM ' . $this->table. " c WHERE c.make="."'".$make."' and c.model="."'".$model."' and c.mileage<="."'".$mileage."' and c.price<="."'".$price."' and c.date_<="."'".$registration."'";
+            $stmt=$db->prepare($query);
+        }
     }
 
 
